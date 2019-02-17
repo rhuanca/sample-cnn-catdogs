a3f440a31d7b3d58c34848c42a0b7df9ebb6a09b
Renan Huanca <renan.huanca@mojix.com>
2019-02-17 11:41:57 -0400

    working...

--- test_model.py
+++ test_model.py
@@ -1,10 +1,21 @@
+from keras.preprocessing import image
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras import backend as K
+import numpy as np
+import sys
 
 
+if len(sys.argv) <= 1:
+    print("Sample usage: ")
+    print("    python3 test_model.py <cat or dot image with jpeg extension>")
+    sys.exit(0)
+
+print("image path: %s" % sys.argv[1])
+
+image_path = sys.argv[1]
 # dimensions of our images.
 img_width, img_height = 150, 150
 
@@ -37,3 +48,17 @@ model.compile(loss='binary_crossentropy',
               optimizer='rmsprop',
               metrics=['accuracy'])
 
+model.load_weights("first_try.h5")
+
+img = image.load_img(image_path, target_size=(img_height, img_width))
+y = image.img_to_array(img)
+y = np.expand_dims(y, axis=0)
+
+images = np.vstack([y])
+classes = model.predict_classes(images, batch_size=10)
+
+print("")
+if classes[0][0] == 1:
+    print ("it is a dog")
+else:
+    print ("it is a cat")
